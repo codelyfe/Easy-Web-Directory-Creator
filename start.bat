a1798bdf05db6ddfd2ca0fb0839c0da999ef1184
codelyfe <codelyfe@gmail.com>
2021-11-18 17:26:28 -0600

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,6 +4,9 @@
 :: 11/18/2021
 :: Made to help create web sites faster.
 
+:: Create CONFIG directory 
+mkdir config
+
 :: Create JS directory 
 mkdir js
 
@@ -20,23 +23,23 @@ mkdir img
 mkdir etc
 
 :: Create index.html file
-set l=
+set l="Erase Me!"
 echo  %l%>index.html
 
 :: Create main.css file in CSS directory
-set l=
+set l="Erase Me!"
 echo  %l%>css/main.css
 
 :: Create main.js file in JS directory
-set l=
+set l="Erase Me!"
 echo  %l%>js/main.js
 
 :: Create index.php file in ETC directory
-set l=
+set l="Erase Me!"
 echo  %l%>etc/index.php
 
 :: Create readme.txt file
-set l=
+set l="Erase Me!"
 echo  %l%>README.txt
 
 :: Create readme.txt file
@@ -44,36 +47,36 @@ set l="Files in this folder are EXTRA please use at own risk . Please check out 
 echo  %l%>etc/README.txt
 
 :: Create .htaccess file for security pref
-set l=
+set l="Erase Me!"
 echo  %l%>.htaccess
 
 :: Create .htpasswd file in ETC directory for security pref
-set l=
+set l="Erase Me!"
 echo  %l%>etc/.htpasswd
 
 :: Create .gitignore file in ETC directory
-set l=
+set l="Erase Me!"
 echo  %l%>etc/.gitignore
 
 :: Create .gitattributes file in ETC directory
-set l=
+set l="Erase Me!"
 echo  %l%>etc/.gitattributes
 
 :: Create .gitkeep file in ETC directory
-set l=
+set l="Erase Me!"
 echo  %l%>etc/.gitkeep
 
 :: Create favicon.ico file
-set l=
+set l="Erase Me!"
 echo  %l%>favicon.ico
 
 :: Create robots.txt file
-set l=
+set l="Erase Me!"
 echo  %l%>robots.txt
 
 :: Create sitemap.xml file
-set l=
+set l="Erase Me!"
 echo  %l%>sitemap.xml
 
 :: Display all files in dir + time
-dir "" > files_in_this_dir.txt 
+dir "" > files_in_this_dir.txt
