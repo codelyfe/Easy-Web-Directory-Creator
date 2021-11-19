fa4892a00245f140fb8d351b47f4940e84f9617f
codelyfe <codelyfe@gmail.com>
2021-11-19 12:04:17 -0600

    Update install-or-update.bat

--- install-or-update.bat
+++ install-or-update.bat
@@ -1,2 +1,5 @@
 :: Installer or Updater
 git clone https://github.com/codelyfe/Easy-Web-Directory-Creator.git
+
+:: Renames Web Directory to www after update or install
+Ren Easy-Web-Directory-Creator www
